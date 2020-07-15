7e4eeda3941ed9d099b366e3bd4d0b7be5ee811e
IncheonPark <dnehd8@gmail.com>
2020-07-15 20:27:39 +0900

    commit. sp_chap08 스프링 DB 연동. PM 08:27

--- src/main/java/spring/MemberDao.java
+++ src/main/java/spring/MemberDao.java
@@ -1,29 +1,90 @@
 package spring;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
 
 public class MemberDao {
 	
-	private static long nextId = 0;
+	private JdbcTemplate jdbcTemplate;
 	
-	private Map<String, Member> map = new HashMap<>(); //<>안의 타입 생략 가능
+	public MemberDao(DataSource dataSource) {
+		this. jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 	
 	public Member selectByEmail(String email) {
-		return map.get(email);
+		List<Member> results = jdbcTemplate.query("select * from MEMBER where EMAIL = ?", new RowMapper<Member>() {
+			@Override
+			public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Member member = new Member(rs.getString("EMAIL"), 
+						rs.getString("PASSWORD"), rs.getString("NAME"), rs.getTimestamp("REGDATE"));
+				member.setId(rs.getLong("ID"));
+				return member;
+			}
+		}, email);
+		
+		return results.isEmpty() ? null : results.get(0);
 	}
 
 	public void insert(Member member) {
-		member.setId(++nextId);
-		map.put(member.getEmail(), member);
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement pstmt = con.prepareStatement ("insert into MEMBER (EMAIL, PASSWORD, NAME, REGDATE) values (?,?,?,?)", new String[] {"ID"});
+				pstmt.setString(1, member.getEmail());
+				pstmt.setString(2, member.getPassword());
+				pstmt.setString(3, member.getName());
+				pstmt.setTimestamp(4, new Timestamp(member.getRegisterDate().getTime()));
+				return pstmt;
+			}
+
+			
+		
+		}, keyHolder);
+		Number keyValue = keyHolder.getKey();
+		member.setId(keyValue.longValue());
+		
 	}
 	
 	public void update(Member member) {
-		map.put(member.getEmail(), member);
+		jdbcTemplate.update("update MEMBER set NAME = ?, PASSWORD = ? where IMAIL = ?", 
+				member.getName(), member.getPassword(), member.getEmail());
+		
+		
 	}
 	
-	public Collection<Member> selectAll() {
-		return map.values(); //map의 value들을 collection으로 반환 한다.
+	//위의 selectByEmail과 코드 중복이 생기는데, 이는 RowMapper클래스를 구현한 클래스를 작성하여 이용하면 된다. MemberRowMapper 클래스 구현 해 놓음.
+	public List<Member> selectAll() {
+		List<Member> results = jdbcTemplate.query("select * from MEMBER", new RowMapper<Member>() {
+			@Override
+			public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Member member = new Member(rs.getString("EMAIL"), 
+						rs.getString("PASSWORD"), rs.getString("NAME"), rs.getTimestamp("REGDATE"));
+				member.setId(rs.getLong("ID"));
+				return member; 
+
+			}
+		});
+		return results;
+	}
+	
+	public int count() {
+		Integer count = jdbcTemplate.queryForObject("select count(*) from MEMBER", Integer.class);
+		return count; 
 	}
 }
